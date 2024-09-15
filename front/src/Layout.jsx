99af22e8554e65008df36493b815478d183a32a0
Andrea Miguel Jarillo <andreamj94@gmail.com>
2024-09-15 21:28:51 +0200

    commit

---
+++ front/src/Layout.jsx
@@ -0,0 +1,17 @@
+import './css/Layout.css'
+import './css/Emails.css'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function Layout() {
+  
+
+  return (
+    <>
+      Hola
+    </>
+  )
+}
+
+export default Layout
